5dce41111b5602821cc7f1b9d92d8194f2362621
SuperCoolGamer <103328500+SuperCoolGamer@users.noreply.github.com>
2022-11-22 18:17:21 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 Webcam.set({
 width:150,
 height:100,
-image_format:'jpeg',
-jpeg_quality:90
+image_format:'png',
+png_quality:90
 });
 
-camera= document.getElementById("Camera");
+camera= document.getElementById("camera");
 
 Webcam.attach('#camera');
 
@@ -14,11 +14,36 @@ function takeimage(){
     Webcam.snap(function(data_uri)
     {
         document.getElementById("Result").innerHTML ='<img id="captured_image"  src="'+data_uri+'"/> ';
+        
     });
 
 }
 
 
+function modelLoaded(){
+ console.log("Model Loaded");
+}
 
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CYX-9zPja/https://teachablemachine.withgoogle.com/models/CYX-9zPja/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/CYX-9zPja/model.json',modelLoaded);
+
+function Check()
+{
+   img = document.getElementById('captured_image');
+   classifier.classify(img, gotResult);
+
+}
+
+
+function gotResult(error, results)
+{
+  if (error) {
+    console.error(error);
+  } else {
+     console.log(results);
+     document.getElementById("object").innerHTML = results[0].label;
+     document.getElementById("Accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+
+
+}
